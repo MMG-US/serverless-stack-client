8f3f3ea521e75871559720e22f79e86088b1dc23
ViGo01 <goelvikash@gmail.com>
2020-06-19 23:33:54 -0700

    commit after login logout

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-2",
+        BUCKET: "notes-app-uploads-vg01"
+    },
+    apiGateway: {
+        REGION: "us-east-2",
+        URL: "https://66ymsuj36k.execute-api.us-east-2.amazonaws.com/prod/"
+    },
+    cognito: {
+        REGION: "us-east-2",
+        USER_POOL_ID: "us-east-2_eTZX41Uny",
+        APP_CLIENT_ID: "6vckrqjosujf71rgh2t7gc0a89",
+        IDENTITY_POOL_ID: "us-east-2:2467af9e-3728-4470-a76d-9c5dbbd3f0bf"
+    }
+};
